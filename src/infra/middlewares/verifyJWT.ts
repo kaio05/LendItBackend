fcb62e7377bb478ab3d4bdec7f8f263ec7ece534
Daniel <danielnoronha57@gmail.com>
2026-04-20 15:53:06 -0300

    fix: some dependencies were on dev dependencies

--- src/infra/middlewares/verifyJWT.ts
+++ src/infra/middlewares/verifyJWT.ts
@@ -4,11 +4,14 @@ import { jwtHelp, JWTVerifyResponse } from "../../app/utils/jwtHelp";
 export const verifyAccessToken = async (req: Request, res: Response, next: NextFunction) => {
     const authHeader = req.headers['authorization'];
     if(!authHeader) return res.sendStatus(401);
+
     const token = authHeader.split(' ')[1];
     const response = await new jwtHelp().verify(
         token,
         process.env.ACCESS_TOKEN_SECRET || "superSecret"
     )
+
     if(response.err) return res.sendStatus(403);
+    
     next();
 }
