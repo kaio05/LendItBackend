59729fbf50faac7da884409472981af427f86236
Kaio Silva <kaiofernando05cont@gmail.com>
2026-05-08 20:47:05 -0300

    feat: create socket

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -1,6 +1,29 @@
 import app from '../app';
 import config from './config/config';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
 
-app.listen(config.port, () => {
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: `http://localhost:${config.port}`,
+    methods: ['GET', 'POST']
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1><script src="/socket.io/socket.io.js"></script><script>const socket = io();</script>')
+})
+
+io.on('connection', (socket) => {
+  console.log('User connected', socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected', socket.id);
+  });
+});
+
+server.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
