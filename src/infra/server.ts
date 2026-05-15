347933330979769087dfa8f6c95cf0408fb2f561
Kaio Silva <kaiofernando05cont@gmail.com>
2026-05-14 23:14:58 -0300

    feat: added socket logic to chat

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -5,6 +5,8 @@ import { createServer } from 'node:http';
 
 const server = createServer(app);
 
+let onlineUsers: {userId: string, socketId: string}[] = []
+
 export const io = new Server(server, {
   cors: {
     origin: `http://localhost:${config.frontendPort}`,
@@ -15,14 +17,33 @@ export const io = new Server(server, {
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 
-  socket.on('send_message', (data) => {
-    console.log('Message received:', data);
-    socket.broadcast.emit('receive_message', data);
+  socket.on('sendMessage', (message) => {
+    const user = onlineUsers.find(user => user.userId === message.recipientId)
+
+    if(user) {
+      io.to(user.socketId).emit("getMessage", message)
+    }
   });
   
   socket.on('disconnect', () => {
     console.log(`User disconnected: ${socket.id}`);
+    onlineUsers = onlineUsers.filter(user => user.socketId !== socket.id)
+
+    console.log("onlineUsers", onlineUsers)
+    io.emit("getOnlineUsers", onlineUsers)
   });
+
+  socket.on("addNewUser", (userId) => {
+    !onlineUsers.some(user => user.userId === userId) &&
+    onlineUsers.push({
+      userId,
+      socketId: socket.id
+    })
+
+    console.log("onlineUsers:", onlineUsers)
+
+    io.emit("getOnlineUsers", onlineUsers)
+  })
 });
 
 server.listen(config.port, () => {
