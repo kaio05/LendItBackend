a908e02f3367578a4dda1e6955f04e81316f0b85
Kaio Silva <kaiofernando05cont@gmail.com>
2026-06-13 00:23:21 -0300

    fix: game code autoincrement

--- src/infra/schemas/gameSchema.ts
+++ src/infra/schemas/gameSchema.ts
@@ -2,7 +2,6 @@ import { z } from "zod";
 import { GameCategories } from "@/domain/entities/game";
 
 export const createGameSchema = z.object({
-    code: z.string(),
     name: z.string(),
     category: z.enum(GameCategories),
     description: z.string(),
